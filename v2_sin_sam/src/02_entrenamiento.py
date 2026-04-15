53a7c1236c21911506fe7a0f01e7e9444caffa6d
DANNY00MX <125932296+Daniel17MX@users.noreply.github.com>
2026-04-14 22:42:06 -0600

    Update 02_entrenamiento.py

--- v2_sin_sam/src/02_entrenamiento.py
+++ v2_sin_sam/src/02_entrenamiento.py
@@ -1,36 +1,32 @@
 import os
 from ultralytics import YOLO
 
-# --- 1. CONFIGURACIÓN DE RUTAS ---
+# --- CONFIGURACIÓN DE RUTAS ---
 PROJECT_ROOT = r"D:\prueba 2\skin-lesion-detector"
-# APUNTAMOS AL DATASET SIN SAM (Bounding Box Completo)
 YAML_PATH = os.path.join(PROJECT_ROOT, "data", "yolo_dataset_sin_sam", "dataset_sin_sam.yaml")
 
 def main():
     print("=== Iniciando Estudio Comparativo: YOLOv11 (SIN SAM) ===")
     
-    # 2. CARGA DEL MODELO
     print("Cargando modelo YOLO11 SMALL (Para una comparación justa con el FINAL)...")
     model = YOLO('yolo11s.pt') 
 
     print("\nIniciando entrenamiento...")
 
-    # 3. EJECUCIÓN DEL ENTRENAMIENTO (MISMOS PARÁMETROS QUE EL FINAL)
     results = model.train(
         # --- Datos y Hardware ---
         data=YAML_PATH,
         epochs=150,                  # Igual que el modelo Con SAM
         patience=25,                 
         imgsz=640,                   
-        batch=8,                     # Protege tu RTX 3050
+        batch=8,                     
         device=0,                    
         workers=4,                   
         
         # --- Guardado de Resultados ---
         project=os.path.join(PROJECT_ROOT, "runs"), 
-        name="detector_lesiones_SIN_SAM", # Carpeta separada para comparar mañana
+        name="detector_lesiones_SIN_SAM", 
         
-        # --- CORRECCIÓN DE DATA AUGMENTATION (Igual que el modelo FINAL) ---
         degrees=0.0,                 
         flipud=0.0,                  
         fliplr=0.5,                  
